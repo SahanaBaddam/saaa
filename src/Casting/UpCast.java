51fc747db13b088af88dc82b92a259c0175257b2
SahanaBaddam <43335918+SahanaBaddam@users.noreply.github.com>
2018-10-02 15:46:40 +0530

    add files

---
+++ src/Casting/UpCast.java
@@ -0,0 +1,13 @@
+package Casting;
+
+public class UpCast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal a=new Tiger();
+		a.run();
+		a.eat();
+
+	}
+
+}
