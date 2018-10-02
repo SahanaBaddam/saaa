3223c09d6b078b3a833d2f0d9bf98fbe19229240
SahanaBaddam <43335918+SahanaBaddam@users.noreply.github.com>
2018-10-02 11:13:13 +0530

    add files

---
+++ src/exceptions/Ex3.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+public class Ex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s=null;
+		try{
+		int a=s.length();
+		}
+		catch(Exception e)
+		{
+			System.out.println("abc");
+		}
+
+	}
+
+}
