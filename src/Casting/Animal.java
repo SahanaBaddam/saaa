baa3c31714254fa5e7c3fa7eb274b4523d3b8302
SahanaBaddam <43335918+SahanaBaddam@users.noreply.github.com>
2018-10-02 15:26:27 +0530

    add files

---
+++ src/Casting/Animal.java
@@ -0,0 +1,15 @@
+package Casting;
+
+public class Animal {
+	
+	public void run()
+	{
+		System.out.println("10kmph");
+	}
+	
+	public  void eat()
+	{
+		System.out.println("eating animal");
+	}
+
+}
---
+++ src/Casting/Tiger.java
@@ -0,0 +1,14 @@
+package Casting;
+
+public class Tiger extends Animal {
+
+	public void run(){
+		System.out.println("15kmpl");
+	}
+	
+	public void eat()
+	{
+		System.out.println("eating tiger");
+	}
+
+}
